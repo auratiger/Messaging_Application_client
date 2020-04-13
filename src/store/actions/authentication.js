258fa0cdf3f26878205a322e2f919d7aa56e870f
unknown <georgi.georgiev@jboxers.com>
2020-04-13 23:55:24 +0300

    jwt authentication working

--- src/store/actions/authentication.js
+++ src/store/actions/authentication.js
@@ -1,6 +1,17 @@
 import axios from 'axios';
 import {GET_ERRORS, SET_CURRENT_USER} from './actionTypes';
+import {setAuthToken} from '../../setAuthToken';
 const qs = require('querystring')
+
+function parseJwt (token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+};
 
 
 export const loginUser = (user) => dispatch => {
@@ -13,7 +24,11 @@ export const loginUser = (user) => dispatch => {
     
     axios.post('http://localhost:8080/ChatRoom/rest/user/login', qs.stringify(user), config)
             .then(res => {
-                console.log(res.data);
+                const token = res.data;
+                localStorage.setItem("jwtToken", token);
+                setAuthToken(token)
+                const decoded = parseJwt(token);             
+                dispatch(setCurrentUser(decoded))
             })
             .catch(err => {
                 dispatch({
@@ -21,6 +36,13 @@ export const loginUser = (user) => dispatch => {
                     payload: err,
                 });
             });
+}
+
+export const setCurrentUser = decoded => {
+    return {
+        type: SET_CURRENT_USER,
+        payload: decoded,
+    }
 }
 
 export const createUser = (user) => dispatch => {
